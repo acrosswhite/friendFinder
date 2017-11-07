38b6575ed10cfc0a5bf7bd9975d7ecde32197d95
Ashley Crosswhite <ar.crosswhite@gmail.com>
2017-11-06 20:24:07 -0800

    update friends file and survey questions

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,18 +3,27 @@ var friends = [
 		name: "ashley crosswhite",
 		photo: "https://vignette.wikia.nocookie.net/despicableme/images/5/5b/F7df36dd257ba4d71a8eb787a0fe1d77.jpeg/revision/latest/scale-to-width-down/250?cb=20160526164942",
 		scores: [
-		5,
-		3]
+		5,3,5,2,3,1,4,5,2,4]
 	},
 	{
 		name: "dave minion",
 		photo: "https://cdn6.bigcommerce.com/s-5ylnei6or5/images/stencil/1280x1280/products/950/1549/2038_Dave_Minion_23__19337.1497284237.jpg?c=2",
 		scores: [
-		5,
-		1]
+		5,1,3,1,1,1,2,2,2,4]
+	},
+	{
+		name: "stuart minion",
+		photo: "http://minions2015.com/wp-content/uploads/2015/10/Stuart_minions.jpg",
+		scores: [
+		2,5,5,4,4,3,5,3,4,5]
+	},
+	{
+		name: "bob minion",
+		photo: "https://vignette4.wikia.nocookie.net/despicableme/images/c/ca/Bob-from-the-minions-movie.jpg/revision/latest?cb=20151224154354",
+		scores: [
+		4,1,1,1,2,3,2,4,5,5]
 	}
 ];
-
 
 
 
